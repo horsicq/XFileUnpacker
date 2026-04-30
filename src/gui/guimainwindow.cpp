4c5f6246ade509c999d1eef3af9bdc715e7a0f4e
horsicq <horsicq@gmail.com>
2026-05-01 00:44:20 +0200

    feat: add initial project structure and resources

---
+++ src/gui/guimainwindow.cpp
@@ -0,0 +1,159 @@
+/* Copyright (c) 2025-2026 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "guimainwindow.h"
+
+#include "ui_guimainwindow.h"
+
+GuiMainWindow::GuiMainWindow(QWidget *pParent) : QMainWindow(pParent), ui(new Ui::GuiMainWindow), g_pFile(nullptr)
+{
+    ui->setupUi(this);
+
+    setWindowTitle(XOptions::getTitle(X_APPLICATIONDISPLAYNAME, X_APPLICATIONVERSION));
+    setAcceptDrops(true);
+
+    g_xOptions.setName(X_OPTIONSFILE);
+    g_xOptions.addID(XOptions::ID_VIEW_STAYONTOP, false);
+    g_xOptions.addID(XOptions::ID_VIEW_STYLE, QStringLiteral("Fusion"));
+    g_xOptions.addID(XOptions::ID_VIEW_LANG, QStringLiteral("System"));
+    g_xOptions.addID(XOptions::ID_VIEW_QSS, QStringLiteral(""));
+    g_xOptions.addID(XOptions::ID_VIEW_FONT_CONTROLS, XOptions::getDefaultFont().toString());
+    g_xOptions.addID(XOptions::ID_VIEW_FONT_TABLEVIEWS, XOptions::getMonoFont().toString());
+    g_xOptions.addID(XOptions::ID_FILE_SAVELASTDIRECTORY, true);
+    g_xOptions.addID(XOptions::ID_FILE_SAVERECENTFILES, true);
+    g_xOptions.load();
+
+    connect(&g_xOptions, SIGNAL(openFile(QString)), this, SLOT(openFile(QString)));
+
+    g_pRecentFilesMenu = g_xOptions.createRecentFilesMenu(this);
+    ui->toolButtonRecentFiles->setEnabled(g_xOptions.getRecentFiles().count());
+
+    adjustView();
+
+    if (QCoreApplication::arguments().count() > 1) {
+        openFile(QCoreApplication::arguments().at(1));
+    }
+}
+
+GuiMainWindow::~GuiMainWindow()
+{
+    g_xOptions.save();
+
+    if (g_pFile) {
+        g_pFile->close();
+        delete g_pFile;
+    }
+
+    delete ui;
+}
+
+void GuiMainWindow::openFile(const QString &sFileName)
+{
+    QFileInfo fi(sFileName);
+    if (!fi.isFile()) {
+        return;
+    }
+
+    if (g_pFile) {
+        g_pFile->close();
+        delete g_pFile;
+        g_pFile = nullptr;
+    }
+
+    g_pFile = new QFile(sFileName);
+    if (!g_pFile->open(QIODevice::ReadOnly)) {
+        delete g_pFile;
+        g_pFile = nullptr;
+        return;
+    }
+
+    QSet<XBinary::FT> stFileTypes = XFormats::getFileTypes(g_pFile, true);
+    XBinary::FT fileType = XBinary::_getPrefFileType(&stFileTypes);
+
+    ui->widgetArchive->setData(fileType, g_pFile);
+
+    setWindowTitle(XOptions::getTitle(X_APPLICATIONDISPLAYNAME, X_APPLICATIONVERSION) +
+                   QStringLiteral(" - ") + fi.fileName());
+
+    g_xOptions.setLastFileName(sFileName);
+    ui->toolButtonRecentFiles->setEnabled(g_xOptions.getRecentFiles().count());
+}
+
+void GuiMainWindow::on_pushButtonOpen_clicked()
+{
+    QString sDirectory = g_xOptions.getLastDirectory();
+    QString sFileName = QFileDialog::getOpenFileName(this, tr("Open file") + QStringLiteral("..."), sDirectory, tr("All files") + QStringLiteral(" (*)"));
+
+    if (!sFileName.isEmpty()) {
+        openFile(sFileName);
+    }
+}
+
+void GuiMainWindow::on_pushButtonAbout_clicked()
+{
+    DialogAbout di(this);
+    di.exec();
+}
+
+void GuiMainWindow::on_pushButtonExit_clicked()
+{
+    this->close();
+}
+
+void GuiMainWindow::on_toolButtonRecentFiles_clicked()
+{
+    g_pRecentFilesMenu->exec(QCursor::pos());
+    ui->toolButtonRecentFiles->setEnabled(g_xOptions.getRecentFiles().count());
+}
+
+void GuiMainWindow::adjustView()
+{
+    if (g_xOptions.isIDPresent(XOptions::ID_VIEW_STAYONTOP)) {
+        g_xOptions.adjustStayOnTop(this);
+    }
+
+    g_xOptions.adjustWidget(this, XOptions::ID_VIEW_FONT_CONTROLS);
+    ui->widgetArchive->adjustView();
+}
+
+void GuiMainWindow::dragEnterEvent(QDragEnterEvent *pEvent)
+{
+    pEvent->acceptProposedAction();
+}
+
+void GuiMainWindow::dragMoveEvent(QDragMoveEvent *pEvent)
+{
+    pEvent->acceptProposedAction();
+}
+
+void GuiMainWindow::dropEvent(QDropEvent *pEvent)
+{
+    const QMimeData *mimeData = pEvent->mimeData();
+
+    if (mimeData->hasUrls()) {
+        QList<QUrl> urlList = mimeData->urls();
+
+        if (urlList.count()) {
+            QString sFileName = urlList.at(0).toLocalFile();
+            sFileName = XBinary::convertFileName(sFileName);
+            openFile(sFileName);
+        }
+    }
+}
---
+++ src/gui/guimainwindow.h
@@ -0,0 +1,68 @@
+/* Copyright (c) 2025-2026 hors<horsicq@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#ifndef GUIMAINWINDOW_H
+#define GUIMAINWINDOW_H
+
+#include <QDragEnterEvent>
+#include <QFile>
+#include <QFileDialog>
+#include <QMainWindow>
+#include <QMenu>
+#include <QMimeData>
+
+#include "../global.h"
+#include "dialogabout.h"
+#include "xformats.h"
+#include "xgenericarchivewidget.h"
+#include "xoptions.h"
+
+namespace Ui {
+class GuiMainWindow;
+}
+
+class GuiMainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    explicit GuiMainWindow(QWidget *pParent = nullptr);
+    ~GuiMainWindow() override;
+
+private slots:
+    void openFile(const QString &sFileName);
+    void on_pushButtonOpen_clicked();
+    void on_pushButtonAbout_clicked();
+    void on_pushButtonExit_clicked();
+    void on_toolButtonRecentFiles_clicked();
+    void adjustView();
+
+protected:
+    void dragEnterEvent(QDragEnterEvent *pEvent) override;
+    void dragMoveEvent(QDragMoveEvent *pEvent) override;
+    void dropEvent(QDropEvent *pEvent) override;
+
+private:
+    Ui::GuiMainWindow *ui;
+    XOptions g_xOptions;
+    QMenu *g_pRecentFilesMenu;
+    QFile *g_pFile;
+};
+
+#endif  // GUIMAINWINDOW_H
